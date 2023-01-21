6b9f875a7f5bf5b54ad8d2dc9f43640701662844
cart-el <64067517+cart-el@users.noreply.github.com>
2023-01-21 16:13:06 +0000

    Update ADS 1 ASS 3.py
    Updated Algorithm for the clustering and fitting task

--- ADS 1 ASS 3.py
+++ ADS 1 ASS 3.py
@@ -5,16 +5,18 @@ Created on Fri Jan 20 01:02:00 2023
 @author: USER
 """
 
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.preprocessing import StandardScaler
 from sklearn import preprocessing
 from sklearn.cluster import KMeans
 from scipy.optimize import curve_fit
 from sklearn.metrics import silhouette_score
 import scipy.optimize as opt
-#import err_ranges as err
+import err_ranges as err
 
 #Define a function to read in the file to be processed
 def tool (data):
@@ -61,7 +63,7 @@ data = data.drop(index=[1,3,6,7,11,27,36,51,52,57,61,62,63,64,65,68,
                                 228,230,231,236,238,240,241,249,255,259,261])
 # print(data)
 
-def clean_data(data):
+def clean_data(data, a, b):
     """
     This function takes a dataframe as input and performs several 
     cleaning tasks on it:
@@ -80,8 +82,8 @@ def clean_data(data):
     data = data.set_index('Country Name')
     
     # rename columns
-    data.columns.values[1] = "GDP/Capita"
-    data.columns.values[0] = "Life Expectancy"
+    data.columns.values[1] = a
+    data.columns.values[0] = b
     
     # print information about the dataframe
     #print(data.info())
@@ -92,25 +94,48 @@ def clean_data(data):
     # return cleaned data
     return data
 
-df_general = clean_data(data)
+df_general = clean_data(data, "GDP/Capita", "Life Expectancy")
 print(df_general)
 
-#Define functiong to normalize data
-def normalize_df(df):
+#Extract the 10 countries with highest GDP/Capita
+df_TopGDPc = GDP_pc.nlargest(10, '2020')
+print(df_TopGDPc)
+
+#Extract the 10 countries with lowest GDP/Capita
+df_BtmGDPc = GDP_pc.nsmallest(10, '2020')
+print(df_BtmGDPc)
+
+#Define a function to plot a bar chart
+def barplot(data, title, ylabel, xlabel, colour):
     """
-    Normalize a given DataFrame using MinMaxScaler.
+    Creates a horizontal bar chart using the provided data, title, 
+    y-axis label, and x-axis label.
     
     Parameters:
-    df: DataFrame to be normalized.
-    :return: Normalized DataFrame.
+        data (pandas DataFrame): Data to be used for the chart. 
+        The first column should be the y-axis labels and the second column 
+        should be the x-axis values.
+        title (str): Title of the chart.
+        ylabel (str): Label for the y-axis.
+        xlabel (str): Label for the x-axis.
+        
+    Returns:
+        None
     """
-    scaler = preprocessing.MinMaxScaler()
-    df_norm = scaler.fit_transform(df)
-    return df_norm
+    plt.barh(data.iloc[:,0], data.iloc[:,1], color=colour)
+    plt.title(title, fontweight='bold')
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    plt.savefig('Barplot.png')
+    plt.show()
 
-#Activate scatter plot function
-df_norm = normalize_df(df_general)
-print(df_norm)
+#Plot bottom 10 countries with GDP/capita
+barplot(df_BtmGDPc, 'Countries With Lowest GDP/Capita (USD)', 
+                 'Countries', 'GDP/Capita (USD)', 'green')
+
+#Plot top 10 countries with GDP/capita
+barplot(df_TopGDPc, 'Countries With Highest GDP/Capita (USD)', 
+                 'Countries', 'GDP/Capita (USD)', 'green')
 
 #Define function to plot a scatter plot
 def scatter_plot(data, x_col, y_col, title, xlabel, ylabel, legend):
@@ -128,14 +153,14 @@ def scatter_plot(data, x_col, y_col, title, xlabel, ylabel, legend):
     ylabel (string): the label for the y-axis
     """
     # extract x and y values from the dataframe
-    x = data[:, x_col]
-    y = data[:, y_col]
+    x = data.iloc[:, x_col]
+    y = data.iloc[:, y_col]
     
     # create scatter plot
     plt.scatter(x, y)
     
     # add title, legend, x-axis label, and y-axis label
-    plt.title(title)
+    plt.title(title, fontweight='bold')
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.legend(legend, loc='lower right', borderaxespad=1)
@@ -148,9 +173,9 @@ def scatter_plot(data, x_col, y_col, title, xlabel, ylabel, legend):
     plt.show()
     
 #Activate scatter plot function
-scatter_plot(df_norm, 1, 0, "Scatter Plot of GDP/Capita vs Life Expectancy", 
+scatter_plot(df_general, 1, 0, "Scatter Plot of GDP/Capita vs Life Expectancy", 
              "GDP/Capita", "Life Expectancy", "Countries")
-    
+
 #Filter the dataframe to extract the desired countries
 df1 = df_general[df_general.iloc[:,1] <= 11000] #countries below world average
 print(df1)
@@ -162,14 +187,15 @@ print(df3)
 
 #Plot a scatter plot of countries with a gdp per capita 
 #below the world avergae, i.e < 11,000
-#Normalise the data
-df1_norm = normalize_df(df1)
-print(df1_norm)
-
 #Activate scatter plot function
-scatter_plot(df1_norm, 1, 0, 
-             "Scatter Plot for Countries with GDP/Capita Below World Average", 
-             "GDP/Capita", "Life Expectancy", "Countries")
+scatter_plot(df1, 1, 0, 
+             "Countries with GDP/Capita Below 11,000 (World Average)", 
+             "GDP/Capita (USD)", "Life Expectancy (Years)", "Countries")
+
+#Normalise the data
+scaler = preprocessing.MinMaxScaler()
+df1_norm = scaler.fit_transform(df1)
+print(df1_norm)
 
 #Define a function to determine the number of effective clusters for KMeans
 def optimise_k_num(data, max_k):
@@ -184,7 +210,6 @@ def optimise_k_num(data, max_k):
     
     Returns: None
     """
-        
     means = []
     inertias = []
     
@@ -208,12 +233,12 @@ def optimise_k_num(data, max_k):
     plt.show()
     
     return fig
-#
+
+#Activate the optimum k function to get the number of effective clusters
 optimise_k_num(df1_norm, 10)
 
-
 #Create a function to run the KMeans model on the dataset
-def kmeans_clustering(data, n_clusters):
+def kmeans_model(data, n_clusters):
     """
     Applies K-Means clustering on the data and returns the cluster labels.
 
@@ -229,19 +254,23 @@ def kmeans_clustering(data, n_clusters):
     kmeans = KMeans(n_clusters=n_clusters, init='k-means++', max_iter=100, 
                     random_state=0)
     kmeans.fit(data)
-    labels = kmeans.predict(data)
+    clusters = kmeans.predict(data)
     centroids = kmeans.cluster_centers_
     inertia = kmeans.inertia_
     
-    return labels, centroids, inertia
+    return clusters, centroids, inertia
 
 #Activate KMeans clustering function
-labels, centroids, inertia = kmeans_clustering(df1_norm, 3)
-print("Labels: ", labels)
+clusters, centroids, inertia = kmeans_model(df1, 3)
+print("Clusters: ", clusters)
 print("Centroids: ", centroids)
 print("Inertia: ", inertia)
 
-df1['Clusters'] = labels
+#Calculate the silhouette score for the number of clusters
+sil_0 = silhouette_score(df1_norm, clusters)
+print(sil_0)
+
+df1['Clusters'] = clusters
 print(df1)
 
 #Define a function that plots the clusters
@@ -256,53 +285,101 @@ def plot_clusters(df, cluster, centroids):
     centroids: Array or DataFrame containing the coordinates of the 
     cluster centroids.
     """
-    df[:,1]
-    df[:,0]
+    
+    df.iloc[:,1]
+    df.iloc[:,0]
     cent1 = centroids[:,1]
     cent2 = centroids[:,0]
-    plt.scatter(df[cluster == 0, 1], df[cluster == 0, 0], s=50,
+    plt.scatter(df.iloc[cluster == 0, 1], df.iloc[cluster == 0, 0], s=50,
                c='blue', label='Cluster 0')
-    plt.scatter(df[cluster == 1, 1], df[cluster == 1, 0], s=50,
+    plt.scatter(df.iloc[cluster == 1, 1], df.iloc[cluster == 1, 0], s=50,
                c='orange', label='Cluster 1')
-    plt.scatter(df[cluster == 2, 1], df[cluster == 2, 0], s=50,
+    plt.scatter(df.iloc[cluster == 2, 1], df.iloc[cluster == 2, 0], s=50,
                c='green', label='Cluster 2')
     #Centroid plot
     plt.scatter(cent1, cent2, c='red', s=100, label='Centroid')
-    plt.title('Clustering of Countires with GDP/Capita Below World Average')
+    plt.title('Cluster of Countries with GDP/Capita Below 11,000 (World Average)',
+              fontweight='bold')
     plt.ylabel('Life Expectancy', fontsize=12)
     plt.xlabel('GDP/Capita', fontsize=12)
     plt.legend()
     plt.tight_layout()
+    plt.savefig("Clusters.png")
     plt.show()
 
 #Activate the function to plot the clusters    
-plot_clusters(df1_norm, labels, centroids)
+plot_clusters(df1, clusters, centroids)
+
+#Carry out cluster analysis by plotting a bar chart showing the country 
+#distribution in each cluster
+sns.countplot(x='Clusters', data=df1)
+plt.savefig('Cluster distribution.png')
+plt.title('Cluster Distribution of Countries with GDP/Capita Below'
+    ' (World Average)', fontweight='bold')
+plt.show()
 
 #Define a polynomial function to plot a curve fit curve
-def fit_polynomial(x, y):
+def fit_polynomial(x, a, b, c, d):
     """
     Fit a polynomial of degree 3 to a given set of data points.
     
     Parameters: 
     x: x-coordinates of the data points.
-    y: y-coordinates of the data points.
+    a,b,c,d: function coefficients.
     
     Returns: Optimal values for the coefficients of the polynomial.
     """
-    popt, pcov = curve_fit(fit_polynomial, x, y)
-    return popt
+    #popt, pcov = curve_fit(fit_polynomial, x, y)
+    return  a*x**3 + b*x**2 + c*x + d
 
+#Initialise variables
 x_axis = df1.values[:,1]
 y_axis = df1.values[:,0]
 
+#Instantiate the curvefit function
 popt, pcov = opt.curve_fit(fit_polynomial, x_axis, y_axis)
 a, b, c, d = popt
 print('y = %.5f * x^3 + %.5f * x^2 + %.5f * x + %.5f' % (a, b, c, d))
+#print(pcov)
 
-dec = df1.values[:,1]
-x_line = np.arange(min(dec), max(dec)+1, 1)
-y_line = fit_polynomial(x_line, a, b, c, d)
+#Generate the curvefit line variables
+d_arr = df1.values[:,1] #convert data to an array
+x_line = np.arange(min(d_arr), max(d_arr)+1, 1) #a random range of points
+y_line = fit_polynomial(x_line, a, b, c, d) #generate y-axis variables 
+plt.scatter(x_axis, y_axis, label="Countries") #scatterplot
+#plot the curvefit line
+plt.plot(x_line, y_line, '--', color='black', linewidth=3, label="Curvefit")
+plt.title('Cluster of Countries showing Prediction Line (Curvefit)',
+              fontweight='bold')
+plt.ylabel('Life Expectancy (Years)', fontsize=12)
+plt.xlabel('GDP/Capita (USD)', fontsize=12)
+plt.legend(loc='lower right')
+plt.annotate('y = 0.00671x + 58.308', (3000, 55), fontweight='bold')
+plt.savefig("Scatterplot Prediction Line.png")
+plt.show()
 
-plt.scatter(x_axis, y_axis)
-plt.plot(x_line, y_line, '--', color='black')
+#Generate the confidence interval and error range
+sigma = np.sqrt(np.diag(pcov))
+low, up = err.err_ranges(d_arr, fit_polynomial, popt, sigma)
+#print(low, up)
+#print(pcov) 
+
+ci = 1.95 * np.std(y_axis)/np.sqrt(len(x_axis))
+lower = y_line - ci
+upper = y_line + ci
+print(f'Confidence Interval, ci = {ci}')
+
+#plot showing best fitting function and the error range
+plt.scatter(x_axis, y_axis, label="Countries")
+plt.plot(x_line, y_line, '--', color='black', linewidth=3, 
+         label="Curvefit")
+plt.fill_between(x_line, lower, upper, alpha=0.3, color='green', 
+                 label="Error range")
+plt.title('Cluster Showing Prediction Line (Curvefit) & Error Range',
+              fontweight='bold')
+plt.ylabel('Life Expectancy (Years)', fontsize=12)
+plt.xlabel('GDP/Capita (USD)', fontsize=12)
+plt.annotate(f'C.I = {ci.round(3)}', (7800, 60), fontweight='bold')
+plt.legend(loc='lower right')
+plt.savefig("Scatterplot Prediction Line.png")
 plt.show()
